fe3e6238387aad4dc3f419b9b54c7d7dd47d3257
t-kusakabe <ttmac0704@gmail.com>
2020-05-13 11:06:54 +0900

    Modify nginx conf

--- docker/nginx/envsubst.sh
+++ docker/nginx/envsubst.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '$$SERVER_NAME $$PHP_CONTAINER' < /etc/nginx/conf.d/backend.conf.template > /etc/nginx/conf.d/backend.conf
+envsubst '$$SERVER_NAME $$PHP_CONTAINER $$NODE_CONTAINER' < /etc/nginx/conf.d/backend.conf.template > /etc/nginx/conf.d/backend.conf
 envsubst '$$SERVER_NAME $$NODE_CONTAINER' < /etc/nginx/conf.d/frontend.conf.template > /etc/nginx/conf.d/frontend.conf
